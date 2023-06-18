b8b70ae09397674e2a3b32671b575eb7621a9209
HuangBugWei <67520151+HuangBugWei@users.noreply.github.com>
2023-06-18 14:22:30 +0800

    modified training config file

--- target-maskrcnn-fpn-config.py
+++ target-maskrcnn-fpn-config.py
@@ -17,13 +17,13 @@ model.backbone.bottom_up.freeze_at = 1
 model.roi_heads.num_classes = 2
 model.roi_heads.box_predictor.test_score_thresh = 0.5
 train["init_checkpoint"] = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
-train["output_dir"] = "./ablation-DA-25k"
-model.do_domain = True
+train["output_dir"] = "./ablation-vanilla-235"
+model.do_domain = False
 
-DatasetCatalog.register('steel_train', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-revit-auto-dataset/files-24826.txt", txt=True))
-DatasetCatalog.register('steel_train_target', lambda :  get_no_label_dicts("/home/aicenter/maskrcnn/rebar-target-dataset/imgs"))
+DatasetCatalog.register('steel_train', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-labeled-test-dataset/target-train.txt", txt=True))
 DatasetCatalog.register('steel_test', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-labeled-test-dataset/target-val.txt", txt=True))
-DatasetCatalog.register('steel_test_source', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-revit-auto-dataset/rebar-revit-auto-test.txt", txt=True))
+# DatasetCatalog.register('steel_test_source', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-revit-auto-dataset/target-slab.txt", txt=True))
+DatasetCatalog.register('steel_test_source', lambda :  get_rebar_dicts("/home/aicenter/maskrcnn/rebar-labeled-test-dataset/target-slab.txt", txt=True))
 
 MetadataCatalog.get("steel_train").set(thing_classes=['intersection', 'spacing'])
 MetadataCatalog.get("steel_test").set(thing_classes=['intersection', 'spacing'])
@@ -36,7 +36,7 @@ dataloader.train = LazyCall(build_detection_train_loader)(
                         augmentations=[
                             LazyCall(T.RandomBrightness)(intensity_min=0.4, intensity_max=1.6),
                             LazyCall(T.RandomContrast)(intensity_min=0.4, intensity_max=1.6),
-                            #LazyCall(T.RandomRotation)(angle=[-5, 5], expand=False, center=None, sample_style='range', interp=2),
+                            LazyCall(T.RandomRotation)(angle=[-5, 5], expand=False, center=None, sample_style='range', interp=2),
                             #LazyCall(T.ResizeScale)(min_scale=0.8, max_scale=1.2, target_height=1800, target_width=2400, interp=2),
                             #LazyCall(T.RandomCrop)(crop_type="relative", crop_size=(720/1800, 1280/2400)),
                             LazyCall(T.ResizeShortestEdge)(
@@ -52,33 +52,7 @@ dataloader.train = LazyCall(build_detection_train_loader)(
                     total_batch_size=6,
                     num_workers=4,
                     )
-dataloader.train_target = LazyCall(build_detection_train_loader)(
-                            dataset=LazyCall(get_detection_dataset_dicts)(names="steel_train_target"),
-                            mapper=LazyCall(DatasetMapper)(
-                                is_train=True,
-                                augmentations=[
-                                    LazyCall(T.RandomBrightness)(intensity_min=0.4, intensity_max=1.6),
-                                    LazyCall(T.RandomContrast)(intensity_min=0.4, intensity_max=1.6),
-                                    LazyCall(T.ResizeShortestEdge)(
-                                        short_edge_length=720,
-                                        sample_style="choice",
-                                        max_size=1280,
-                                    ),
-                                    LazyCall(T.RandomCrop)(crop_type="absolute", 
-                                                           crop_size=(720, 960)),
-                                    LazyCall(T.ResizeShortestEdge)(
-                                        short_edge_length=(640, 672, 704, 736, 768, 800),
-                                        sample_style="choice",
-                                        max_size=1333,
-                                    ),
-                                    LazyCall(T.RandomFlip)(horizontal=True),
-                                ],
-                                image_format="BGR",
-                                use_instance_mask=True,
-                            ),
-                            total_batch_size=6,
-                            num_workers=4,
-                            )
+dataloader.train_target = None
 
 dataloader.test = LazyCall(build_detection_test_loader)(
                     dataset=LazyCall(get_detection_dataset_dicts)(names="steel_test", filter_empty=False),
